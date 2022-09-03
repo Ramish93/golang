75e5e050d170d8cffb33e332ea9e8a420fcc1686
Ramish Hassan <ramishshah93@gmail.com>
2022-09-04 01:14:43 +0200

    basic booking tickets app complete

--- main.go
+++ main.go
@@ -3,13 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
+	var conferenceName string = "Go Conference"
 	const conferenceTickets = 50
-	var remainingTickets = 50
+	var remainingTickets uint = 50
 
 	fmt.Printf("Hello to the %v booking app \n", conferenceName)
 	fmt.Printf("we have %v tickets and remaining tickets: %v \n", conferenceTickets, remainingTickets)
-	fmt.Println("you can get your tickets here")
+	fmt.Println(&remainingTickets)
+	fmt.Printf("conN is %T, remT is %T \n", conferenceName, remainingTickets)
 
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+	//as username
+	fmt.Println("enter firstName")
+	fmt.Scan(&firstName)
+	fmt.Println("enter lastName")
+	fmt.Scan(&lastName)
+	fmt.Println("enter email")
+	fmt.Scan(&email)
+	fmt.Println("enter tickets")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+	fmt.Printf("userName %v %v has booked %v tickets email: %v \n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining", remainingTickets)
 	
 }
