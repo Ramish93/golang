ae1dd1c1494b5645e31105a9a2da772e3d82c657
Ramish Hassan <ramishshah93@gmail.com>
2022-09-04 12:09:34 +0200

    added for loops to keep app running and print first name, if statement to end loop when no tickets available.

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var conferenceName string = "Go Conference"
@@ -12,32 +15,47 @@ func main() {
 	fmt.Println(&remainingTickets)
 	fmt.Printf("conN is %T, remT is %T \n", conferenceName, remainingTickets)
 
-	var bookings []string
+	var bookings = []string{}
 
-
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
-	//as username
-	fmt.Println("enter firstName")
-	fmt.Scan(&firstName)
-	fmt.Println("enter lastName")
-	fmt.Scan(&lastName)
-	fmt.Println("enter email")
-	fmt.Scan(&email)
-	fmt.Println("enter tickets")
-	fmt.Scan(&userTickets)
-
-	remainingTickets = remainingTickets - userTickets
+	for {
+		
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+		//as username
+		fmt.Println("enter firstName")
+		fmt.Scan(&firstName)
+		fmt.Println("enter lastName")
+		fmt.Scan(&lastName)
+		fmt.Println("enter email")
+		fmt.Scan(&email)
+		fmt.Println("enter tickets")
+		fmt.Scan(&userTickets)
 	
-	bookings = append(bookings, firstName +" "+ lastName)
-	fmt.Printf("Slice  %v \n", bookings)
-	fmt.Printf("Slice  %v \n", bookings[0])
-	fmt.Printf("Slice  %v \n", len(bookings))
+		remainingTickets = remainingTickets - userTickets
+	
+		bookings = append(bookings, firstName +" "+ lastName)
+		fmt.Printf("Slice  %v \n", bookings)
+		fmt.Printf("Slice  %v \n", bookings[0])
+		fmt.Printf("Slice  %v \n", len(bookings))
+	
+		fmt.Printf("userName %v %v has booked %v tickets email: %v \n", firstName, lastName, userTickets, email)
+		
+		firstNames := []string{}
+		for _, booking := range bookings{
+			var names = strings.Fields(booking)
+			var firstName = names[0]
+			firstNames = append(firstNames, firstName)
+		}
+		fmt.Printf("%v booked first ticket \n", firstNames)
 
-	fmt.Printf("userName %v %v has booked %v tickets email: %v \n", firstName, lastName, userTickets, email)
-
-	fmt.Printf("%v tickets remaining", remainingTickets)
+		var noticks bool = remainingTickets == 0
+		if noticks {
+			// end programme
+			fmt.Println("no more tickets")
+			break
+		}
+	}
 	
 }
